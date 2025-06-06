69e11764d117bceefae96fd3b8f80b1333cbaaff
Scottyp1 <codymoto122@gmail.com>
2025-06-06 15:54:51 -0400

    Add: Two funcs to Show, Add contacts

--- contacts/contacts.py
+++ contacts/contacts.py
@@ -1,1 +1,16 @@
-test
+import csv
+
+
+def show_contacts():
+    with open('contacts.csv', 'r') as contacts:
+        reader = csv.reader(contacts)
+        for rows in reader:
+            print(rows)
+
+
+def add_contacts():
+    name = input('Enter name: ')
+    number = input('Enter number: ')
+    with open('contacts.csv', 'a', newline='') as contacts:
+        writer = csv.writer(contacts)
+        writer.writerow([name, number])
